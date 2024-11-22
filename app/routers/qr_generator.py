cd3df449e7ca3362927f355b7c3d38015df58dd2
simon <simondjv2016@gmail.com>
2024-11-21 23:23:02 -0500

    qr content disposition headers

--- app/routers/qr_generator.py
+++ app/routers/qr_generator.py
@@ -6,4 +6,7 @@ qr_generator_router = APIRouter()
 @qr_generator_router.get("/generate", tags=["QR"])
 def generate_qr(link: str):
 	qr = QRGenerator(link).generate()
-	return Response(content=qr.getvalue(), media_type="image/png")
+	response = Response(content=qr.getvalue(), media_type="image/png")
+	response.headers["Content-Disposition"] = f"attachment; filename=qr_shareit.png"
+	response.headers["Access-Control-Expose-Headers"] = "Content-Disposition"
+	return response
--- app/services/QRGenerator.py
+++ app/services/QRGenerator.py
@@ -8,6 +8,6 @@ class QRGenerator:
 	def generate(self):
 		qr = segno.make(self.link)
 		buff = io.BytesIO()
-		qr.save(buff, kind="png")
+		qr.save(buff, kind="png", scale=10)
 		buff.seek(0)
 		return buff
